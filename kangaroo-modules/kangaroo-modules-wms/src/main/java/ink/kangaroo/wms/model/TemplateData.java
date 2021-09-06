5cda365016e06e6c836a100cf30529770620db0e
Kangaroo <26599115@qq.com>
2021-09-06 19:03:29 +0800

    2021年9月6日19点03分

---
+++ kangaroo-modules/kangaroo-modules-wms/src/main/java/ink/kangaroo/wms/model/TemplateData.java
@@ -0,0 +1,22 @@
+package ink.kangaroo.wms.model;
+
+import ink.kangaroo.common.core.web.domain.BaseEntity;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author kbw
+ * @version 1.0
+ * @date 2021/9/6 9:06
+ */
+@Data
+public class TemplateData extends BaseEntity {
+
+    private Integer id;
+    private Integer instanceId;
+    private String data;
+    private String htmlText;
+    private Long userId;
+
+}
---
+++ kangaroo-modules/kangaroo-modules-wms/src/main/java/ink/kangaroo/wms/model/TemplateInstance.java
@@ -0,0 +1,12 @@
+package ink.kangaroo.wms.model;
+
+import lombok.Data;
+
+/**
+ * @author kbw
+ * @version 1.0
+ * @date 2021/9/6 9:11
+ */
+@Data
+public class TemplateInstance {
+}
